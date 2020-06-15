836af198f8276fb7bc10e5463f6d9df5fca823d3
Delta2Force <deltacommandoforcegames@gmail.com>
2020-06-15 14:07:01 +0200

    Removed fabric key bindings, will directly use GLFW

--- src/main/java/mcvmcomputers/ClientInitializer.java
+++ src/main/java/mcvmcomputers/ClientInitializer.java
@@ -11,26 +11,16 @@ import mcvmcomputers.entities.render.KeyboardRender;
 import mcvmcomputers.entities.render.MouseRender;
 import mcvmcomputers.entities.render.PCRender;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
-import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
-import net.minecraft.client.util.InputUtil.Type;
-import net.minecraft.util.Identifier;
 
 public class ClientInitializer implements ClientModInitializer{
-	public static FabricKeyBinding unfocusKey1 = FabricKeyBinding.Builder.create(new Identifier("mcvmcomputers", "unfocuskey1"), Type.KEYSYM, GLFW.GLFW_KEY_LEFT_CONTROL, "VM Computers").build();
-	public static FabricKeyBinding unfocusKey2 = FabricKeyBinding.Builder.create(new Identifier("mcvmcomputers", "unfocuskey2"), Type.KEYSYM, GLFW.GLFW_KEY_RIGHT_CONTROL, "VM Computers").build();
-	public static FabricKeyBinding unfocusKey3 = FabricKeyBinding.Builder.create(new Identifier("mcvmcomputers", "unfocuskey3"), Type.KEYSYM, GLFW.GLFW_KEY_BACKSPACE, "VM Computers").build();
-	public static FabricKeyBinding unfocusKey4 = FabricKeyBinding.Builder.create(new Identifier("mcvmcomputers", "unfocuskey4"), Type.KEYSYM, -1, "VM Computers").build();
+	public static int glfwUnfocusKey1 = GLFW.GLFW_KEY_LEFT_CONTROL;
+	public static int glfwUnfocusKey2 = GLFW.GLFW_KEY_RIGHT_CONTROL;
+	public static int glfwUnfocusKey3 = GLFW.GLFW_KEY_BACKSPACE;
+	public static int glfwUnfocusKey4 = -1;
 	
 	@Override
 	public void onInitializeClient() {
-		KeyBindingRegistry.INSTANCE.addCategory("VM Computers");
-		KeyBindingRegistry.INSTANCE.register(unfocusKey1);
-		KeyBindingRegistry.INSTANCE.register(unfocusKey2);
-		KeyBindingRegistry.INSTANCE.register(unfocusKey3);
-		KeyBindingRegistry.INSTANCE.register(unfocusKey4);
-		
 		EntityRendererRegistry.INSTANCE.register(EntityList.ITEM_PREVIEW,
 				(entityRenderDispatcher, context) -> new ItemPreviewRender(entityRenderDispatcher));
 		EntityRendererRegistry.INSTANCE.register(EntityList.KEYBOARD,
