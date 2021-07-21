46799c6d9b8d6ed3e113d6a537bd0835d81d413c
Delta2Force <deltacommandoforcegames@gmail.com>
2021-07-21 22:06:21 +0200

    jni: changed mingw gcc flags

--- jni/build_library.sh
+++ jni/build_library.sh
@@ -18,15 +18,15 @@ if [ ! -d bin ]; then
 fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	gcc -g -Wall -c -fPIC -I$JAVA_DIR/include -I$JAVA_DIR/include/linux -Isdk/bindings/c/include -Isdk/bindings/xpcom/include -Isdk/bindings/c/glue src/c/vbhook_VBHook.c -o bin/vbhook.o
 	gcc -g -Wall -c -fPIC -Isdk/bindings/c/include -Isdk/bindings/xpcom/include -Isdk/bindings/c/glue sdk/bindings/c/glue/VBoxCAPIGlue.c -o bin/VBoxCAPIGlue.o
 	gcc -g -Wall -c -fPIC -Isdk/bindings/c/include -Isdk/bindings/xpcom/include -Isdk/bindings/c/glue sdk/bindings/xpcom/lib/VirtualBox_i.c -o bin/VirtualBox_i.o
+	gcc -g -Wall -c -fPIC -I$JAVA_DIR/include -I$JAVA_DIR/include/linux -Isdk/bindings/c/include -Isdk/bindings/xpcom/include -Isdk/bindings/c/glue src/c/vbhook_VBHook.c -o bin/vbhook.o
 	gcc -g -Wall -shared -fPIC -o bin/libvbhook.so bin/VirtualBox_i.o bin/VBoxCAPIGlue.o bin/vbhook.o -lc
 	rm bin/*.o
 elif [[ "$OSTYPE" == "msys" ]]; then
-	gcc -g -Wall -I"$JAVA_DIR/include" -I"$JAVA_DIR/include/win32" -Isdk/bindings/c/include -Isdk/bindings/mscom/include -Isdk/bindings/c/glue src/c/vbhook_VBHook.c -o bin/vbhook.o
-	gcc -g -Wall -Isdk/bindings/c/include -Isdk/bindings/mscom/include -Isdk/bindings/c/glue sdk/bindings/c/glue/VBoxCAPIGlue.c -o bin/VBoxCAPIGlue.o
-	gcc -g -Wall -Isdk/bindings/c/include -Isdk/bindings/mscom/include -Isdk/bindings/c/glue sdk/bindings/mscom/lib/VirtualBox_i.c -o bin/VirtualBox_i.o
+	gcc -g -Wall -c -Isdk/bindings/c/include -Isdk/bindings/mscom/include -Isdk/bindings/c/glue sdk/bindings/c/glue/VBoxCAPIGlue.c -o bin/VBoxCAPIGlue.o
+	gcc -g -Wall -c -Isdk/bindings/c/include -Isdk/bindings/mscom/include -Isdk/bindings/c/glue sdk/bindings/mscom/lib/VirtualBox_i.c -o bin/VirtualBox_i.o
+	gcc -g -Wall -c -I"$JAVA_DIR/include" -I"$JAVA_DIR/include/win32" -Isdk/bindings/c/include -Isdk/bindings/mscom/include -Isdk/bindings/c/glue src/c/vbhook_VBHook.c -o bin/vbhook.o
 	gcc -g -Wall -shared -o bin/libvbhook.dll bin/VirtualBox_i.o bin/VBoxCAPIGlue.o bin/vbhook.o -lc
 	rm bin/*.o
 fi
