64533a18db557d17ab4fbd43a5eb212c164cbdef
Delta2Force <deltacommandoforcegames@gmail.com>
2021-07-21 03:26:34 +0200

    jni: changed UTF-8 to UTF-16 string conversion
    Yeah the method before didn't really do it properly. Thought this might be the reason behind a few issues because I looked at the VM configs and there were chinese characters.

--- jni/src/c/vbhook_VBHook.c
+++ jni/src/c/vbhook_VBHook.c
@@ -42,9 +42,6 @@ JNIEXPORT void JNICALL Java_vbhook_VBHook_free_1vb_1client(JNIEnv* env, jobject 
 
 JNIEXPORT jboolean JNICALL Java_vbhook_VBHook_init_1glue(JNIEnv* env, jobject obj, jstring vbox_home) {
 	setenv("VBOX_APP_HOME", (*env)->GetStringUTFChars(env, vbox_home, NULL), 1);
-	const char* name;
-	name = getenv("VBOX_APP_HOME");
-	printf("Trying VirtualBox with %s\n", name);
 	return !VBoxCGlueInit();
 }
 
@@ -68,7 +65,10 @@ JNIEXPORT jlong JNICALL Java_vbhook_VBHook_find_1or_1create_1vm(JNIEnv* env, job
 	IVirtualBox_FindMachine(virtualbox, vbname, &vm);
 	if(!vm) {
 		//create if not found
-		virtualbox->lpVtbl->CreateMachine(virtualbox, (PRUnichar*)"", vbname, 0, NULL, vbos_type, (PRUnichar*)"forceOverwrite=1", &vm);
+		BSTR empty; g_pVBoxFuncs->pfnUtf8ToUtf16("", &empty);
+		BSTR options; g_pVBoxFuncs->pfnUtf8ToUtf16("forceOverwrite=1", &options);
+		virtualbox->lpVtbl->CreateMachine(virtualbox, empty, vbname, 0, NULL, vbos_type, options, &vm);
+		g_pVBoxFuncs->pfnUtf16Free(empty); g_pVBoxFuncs->pfnUtf16Free(options);
 		IVirtualBox_RegisterMachine(virtualbox, vm);
 	}
 	return (long)vm;
@@ -78,7 +78,7 @@ JNIEXPORT void JNICALL Java_vbhook_VBHook_vm_1values(JNIEnv* env, jobject obj, j
 	ISession* sessionvb = (ISession*)session;
 	IVirtualBox* vbvb = (IVirtualBox*)vb;
 	IMachine* vmvb = (IMachine*)vm;
-
+	
 	IMachine_LockMachine(vmvb, sessionvb, LockType_Write);
 	IMachine* edit = NULL;
 	ISession_GetMachine(sessionvb, &edit);
@@ -92,38 +92,46 @@ JNIEXPORT void JNICALL Java_vbhook_VBHook_vm_1values(JNIEnv* env, jobject obj, j
 	IGraphicsAdapter_SetVRAMSize(adapter, vram);
 	IGraphicsAdapter_Release(adapter); //we're done with it
 	
-	IMachine_RemoveStorageController(edit, (PRUnichar*)"IDE Controller");
+	BSTR storage_controller_name;
+	g_pVBoxFuncs->pfnUtf8ToUtf16("IDE Controller", &storage_controller_name);
+
+	IMachine_RemoveStorageController(edit, storage_controller_name);
 	IStorageController* storage_controller = NULL;
-	IMachine_AddStorageController(edit, (PRUnichar*)"IDE Controller", StorageBus_IDE, &storage_controller);
-	IStorageController_Release(storage_controller); //this code seems redundant, should play with later
+	IMachine_AddStorageController(edit, storage_controller_name, StorageBus_IDE, &storage_controller);
+	IStorageController_Release(storage_controller);
 	
 	if((*env)->GetStringLength(env, hdd) > 0) {
 		IMedium* hdd_medium = NULL;
 		IVirtualBox_OpenMedium(vbvb, (PRUnichar*)(*env)->GetStringChars(env, hdd, NULL), DeviceType_HardDisk, AccessMode_ReadWrite, PR_TRUE, &hdd_medium);
-		IMachine_AttachDevice(edit, (PRUnichar*)"IDE Controller", 0, 0, DeviceType_HardDisk, hdd_medium);
+		IMachine_AttachDevice(edit, storage_controller_name, 0, 0, DeviceType_HardDisk, hdd_medium);
 		IMedium_Release(hdd_medium);
 	}
-
+	
 	if((*env)->GetStringLength(env, iso) > 0) {
 		IMedium* iso_medium = NULL;
 		IVirtualBox_OpenMedium(vbvb, (PRUnichar*)(*env)->GetStringChars(env, iso, NULL), DeviceType_DVD, AccessMode_ReadOnly, PR_TRUE, &iso_medium);
-		IMachine_AttachDevice(edit, (PRUnichar*)"IDE Controller", 1, 0, DeviceType_DVD, iso_medium);
+		IMachine_AttachDevice(edit, storage_controller_name, 1, 0, DeviceType_DVD, iso_medium);
 		IMedium_Release(iso_medium);
 	}else{
-		IMachine_AttachDevice(edit, (PRUnichar*)"IDE Controller", 1, 0, DeviceType_DVD, NULL);
+		IMachine_AttachDevice(edit, storage_controller_name, 1, 0, DeviceType_DVD, NULL);
 	}
 
 	IMachine_SaveSettings(edit);
 	ISession_UnlockMachine(sessionvb);
 	IMachine_Release(edit);
+	g_pVBoxFuncs->pfnUtf16Clear(storage_controller_name);
 }
 
 JNIEXPORT void JNICALL Java_vbhook_VBHook_start_1vm(JNIEnv* env, jobject obj, jlong session, jlong vm) {
 	ISession* sessionvb = (ISession*)session;
 	IMachine* vmvb = (IMachine*)vm;
 	
+	BSTR headless; g_pVBoxFuncs->pfnUtf8ToUtf16("headless", &headless);
+
 	IProgress* progress = NULL;
-	vmvb->lpVtbl->LaunchVMProcess(vmvb, sessionvb, (PRUnichar*)"headless", 0, NULL, &progress);
+	vmvb->lpVtbl->LaunchVMProcess(vmvb, sessionvb, headless, 0, NULL, &progress);
 	IProgress_WaitForCompletion(progress, -1);
 	IProgress_Release(progress);
+
+	g_pVBoxFuncs->pfnUtf16Free(headless);
 }
