e11c7e6b4c1baa5b13bbaa6f4cba11ad1cf534d2
Delta2Force <deltacommandoforcegames@gmail.com>
2021-07-30 01:35:27 +0200

    jni: added oleaut32.lib to msvc link for safearray functions
    Played around with SAFEARRAYs and had to include it, might remove in the future if not necessary

--- jni/build_library.bat
+++ jni/build_library.bat
@@ -15,7 +15,7 @@ REM Compile & Link
 cl /c /Isdk/bindings/c/include /Isdk/bindings/mscom/include /Isdk/bindings/c/glue /Fobin/VBoxCAPIGlue.obj sdk/bindings/c/glue/VBoxCAPIGlue.c 
 cl /c /Isdk/bindings/c/include /Isdk/bindings/mscom/include /Isdk/bindings/c/glue /Fobin/VirtualBox_i.obj sdk/bindings/mscom/lib/VirtualBox_i.c
 cl /c /I"%JAVA_HOME%/include" /I"%JAVA_HOME%/include/win32" /Isdk/bindings/c/include /Isdk/bindings/mscom/include /Isdk/bindings/c/glue /Fobin/vbhook.obj src/c/vbhook_VBHook.c
-link /dll /OUT:bin/vbhook.dll bin/VirtualBox_i.obj bin/VBoxCAPIGlue.obj bin/vbhook.obj
+link /dll /OUT:bin/vbhook.dll bin/VirtualBox_i.obj bin/VBoxCAPIGlue.obj bin/vbhook.obj oleaut32.lib
 
 REM Remove Residue
 del bin\*.obj 2>NUL
