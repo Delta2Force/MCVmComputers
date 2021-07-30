0c91750d7ebcb09886e42f5fc8f0452d25d761ac
Delta2Force <deltacommandoforcegames@gmail.com>
2021-07-31 00:16:06 +0200

    jni: fixed package.sh for older java versions

--- jni/package.sh
+++ jni/package.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp -r bin src/java/vbhook-libs
-jar cvf ../lib/vbhook.jar -C src/java vbhook/VBHook.class vbhook-libs/libvbhook.so vbhook-libs/vbhook.dll
+jar cvf ../lib/vbhook.jar -C src/java vbhook/VBHook.class -C src/java vbhook-libs/libvbhook.so -C src/java vbhook-libs/vbhook.dll
 rm -r src/java/vbhook-libs
