47165f9dfa03d9d6e996551cf83314b6a98d7702
Delta2Force <deltatwoforce@outlook.com>
2022-12-14 14:08:02 +0100

    Improved KeyConverter and server-side order process code
    Much nicer to look at now

--- src/main/java/mcvmcomputers/client/utils/KeyConverter.java
+++ src/main/java/mcvmcomputers/client/utils/KeyConverter.java
@@ -26,214 +26,117 @@ public class KeyConverter {
 		}
 		return ints;
 	}
-	
-	//is this good practice? it seems absolutely horrendous to me but
-	//i dont know how else to solve it
+
 	public static int[] scancodeFromGLFWKey(int glfwKey) {
-		//US Layout only
-		//Missing some keys
-		switch(glfwKey) {
-		case GLFW_KEY_ESCAPE:
-			return new int[] {0x01};
-		case GLFW_KEY_1:
-			return new int[] {0x02};
-		case GLFW_KEY_2:
-			return new int[] {0x03};
-		case GLFW_KEY_3:
-			return new int[] {0x04};
-		case GLFW_KEY_4:
-			return new int[] {0x05};
-		case GLFW_KEY_5:
-			return new int[] {0x06};
-		case GLFW_KEY_6:
-			return new int[] {0x07};
-		case GLFW_KEY_7:
-			return new int[] {0x08};
-		case GLFW_KEY_8:
-			return new int[] {0x09};
-		case GLFW_KEY_9:
-			return new int[] {0x0a};
-		case GLFW_KEY_0:
-			return new int[] {0x0b};
-		case GLFW_KEY_MINUS:
-			return new int[] {0x0c};
-		case GLFW_KEY_EQUAL:
-			return new int[] {0x0d};
-		case GLFW_KEY_BACKSPACE:
-			return new int[] {0x0e};
-		case GLFW_KEY_TAB:
-			return new int[] {0x0f};
-		case GLFW_KEY_Q:
-			return new int[] {0x10};
-		case GLFW_KEY_W:
-			return new int[] {0x11};
-		case GLFW_KEY_E:
-			return new int[] {0x12};
-		case GLFW_KEY_R:
-			return new int[] {0x13};
-		case GLFW_KEY_T:
-			return new int[] {0x14};
-		case GLFW_KEY_Y:
-			return new int[] {0x15};
-		case GLFW_KEY_U:
-			return new int[] {0x16};
-		case GLFW_KEY_I:
-			return new int[] {0x17};
-		case GLFW_KEY_O:
-			return new int[] {0x18};
-		case GLFW_KEY_P:
-			return new int[] {0x19};
-		case GLFW_KEY_LEFT_BRACKET:
-			return new int[] {0x1a};
-		case GLFW_KEY_RIGHT_BRACKET:
-			return new int[] {0x1b};
-		case GLFW_KEY_ENTER:
-			return new int[] {0x1c};
-		case GLFW_KEY_LEFT_CONTROL:
-			return new int[] {0x1d};
-		case GLFW_KEY_A:
-			return new int[] {0x1e};
-		case GLFW_KEY_S:
-			return new int[] {0x1f};
-		case GLFW_KEY_D:
-			return new int[] {0x20};
-		case GLFW_KEY_F:
-			return new int[] {0x21};
-		case GLFW_KEY_G:
-			return new int[] {0x22};
-		case GLFW_KEY_H:
-			return new int[] {0x23};
-		case GLFW_KEY_J:
-			return new int[] {0x24};
-		case GLFW_KEY_K:
-			return new int[] {0x25};
-		case GLFW_KEY_L:
-			return new int[] {0x26};
-		case GLFW_KEY_SEMICOLON:
-			return new int[] {0x27};
-		case GLFW_KEY_APOSTROPHE:
-			return new int[] {0x28};
-		case GLFW_KEY_GRAVE_ACCENT:
-			return new int[] {0x29};
-		case GLFW_KEY_LEFT_SHIFT:
-			return new int[] {0x2a};
-		case GLFW_KEY_BACKSLASH:
-			return new int[] {0x2b};
-		case GLFW_KEY_Z:
-			return new int[] {0x2c};
-		case GLFW_KEY_X:
-			return new int[] {0x2d};
-		case GLFW_KEY_C:
-			return new int[] {0x2e};
-		case GLFW_KEY_V:
-			return new int[] {0x2f};
-		case GLFW_KEY_B:
-			return new int[] {0x30};
-		case GLFW_KEY_N:
-			return new int[] {0x31};
-		case GLFW_KEY_M:
-			return new int[] {0x32};
-		case GLFW_KEY_COMMA:
-			return new int[] {0x33};
-		case GLFW_KEY_PERIOD:
-			return new int[] {0x34};
-		case GLFW_KEY_SLASH:
-			return new int[] {0x35};
-		case GLFW_KEY_RIGHT_SHIFT:
-			return new int[] {0x36};
-		case GLFW_KEY_KP_MULTIPLY:
-			return new int[] {0x37};
-		case GLFW_KEY_LEFT_ALT:
-			return new int[] {0x38};
-		case GLFW_KEY_SPACE:
-			return new int[] {0x39};
-		case GLFW_KEY_CAPS_LOCK:
-			return new int[] {0x3a};
-		case GLFW_KEY_F1:
-			return new int[] {0x3b};
-		case GLFW_KEY_F2:
-			return new int[] {0x3c};
-		case GLFW_KEY_F3:
-			return new int[] {0x3d};
-		case GLFW_KEY_F4:
-			return new int[] {0x3e};
-		case GLFW_KEY_F5:
-			return new int[] {0x3f};
-		case GLFW_KEY_F6:
-			return new int[] {0x40};
-		case GLFW_KEY_F7:
-			return new int[] {0x41};
-		case GLFW_KEY_F8:
-			return new int[] {0x42};
-		case GLFW_KEY_F9:
-			return new int[] {0x43};
-		case GLFW_KEY_F10:
-			return new int[] {0x44};
-		case GLFW_KEY_NUM_LOCK:
-			return new int[] {0x45};
-		case GLFW_KEY_SCROLL_LOCK:
-			return new int[] {0x46};
-		case GLFW_KEY_KP_7:
-			return new int[] {0x47};
-		case GLFW_KEY_KP_8:
-			return new int[] {0x48};
-		case GLFW_KEY_KP_9:
-			return new int[] {0x49};
-		case GLFW_KEY_KP_SUBTRACT:
-			return new int[] {0x4a};
-		case GLFW_KEY_KP_4:
-			return new int[] {0x4b};
-		case GLFW_KEY_KP_5:
-			return new int[] {0x4c};
-		case GLFW_KEY_KP_6:
-			return new int[] {0x4d};
-		case GLFW_KEY_KP_ADD:
-			return new int[] {0x4e};
-		case GLFW_KEY_KP_1:
-			return new int[] {0x4f};
-		case GLFW_KEY_KP_2:
-			return new int[] {0x50};
-		case GLFW_KEY_KP_3:
-			return new int[] {0x51};
-		case GLFW_KEY_KP_0:
-			return new int[] {0x52};
-		case GLFW_KEY_KP_DECIMAL:
-			return new int[] {0x53};
-		case GLFW_KEY_F11:
-			return new int[] {0x57};
-		case GLFW_KEY_F12:
-			return new int[] {0x58};
-		case GLFW_KEY_DOWN:
-			return new int[] {0xe0, 0x50};
-		case GLFW_KEY_UP:
-			return new int[] {0xe0, 0x48};
-		case GLFW_KEY_LEFT:
-			return new int[] {0xe0, 0x4b};
-		case GLFW_KEY_RIGHT:
-			return new int[] {0xe0, 0x4d};
-		case GLFW_KEY_RIGHT_ALT:
-			return new int[] {0xe0, 0x38};
-		case GLFW_KEY_KP_ENTER:
-			return new int[] {0xe0, 0x1c};
-		case GLFW_KEY_RIGHT_CONTROL:
-			return new int[] {0xe0, 0x1d};
-		case GLFW_KEY_KP_DIVIDE:
-			return new int[] {0xe0, 0x35};
-		case GLFW_KEY_HOME:
-			return new int[] {0xe0, 0x47};
-		case GLFW_KEY_PAGE_UP:
-			return new int[] {0xe0, 0x49};
-		case GLFW_KEY_END:
-			return new int[] {0xe0, 0x50};
-		case GLFW_KEY_PAGE_DOWN:
-			return new int[] {0xe0, 0x51};
-		case GLFW_KEY_INSERT:
-			return new int[] {0xe0, 0x52};
-		case GLFW_KEY_DELETE:
-			return new int[] {0xe0, 0x53};
-		case GLFW_KEY_MENU:
-			return new int[] {0xe0, 0x5d};
+		if(scancodes[glfwKey] == null) {
+			return new int[] {0x00};
 		}
-		return new int[] {0x00};
+
+		return scancodes[glfwKey];
+	}
+
+	private static final int[][] scancodes = new int[GLFW_KEY_LAST + 1][];
+
+	static {
+		scancodes[GLFW_KEY_ESCAPE] = new int[] {0x01};
+		scancodes[GLFW_KEY_1] = new int[] {0x02};
+		scancodes[GLFW_KEY_2] = new int[] {0x03};
+		scancodes[GLFW_KEY_3] = new int[] {0x04};
+		scancodes[GLFW_KEY_4] = new int[] {0x05};
+		scancodes[GLFW_KEY_5] = new int[] {0x06};
+		scancodes[GLFW_KEY_6] = new int[] {0x07};
+		scancodes[GLFW_KEY_7] = new int[] {0x08};
+		scancodes[GLFW_KEY_8] = new int[] {0x09};
+		scancodes[GLFW_KEY_9] = new int[] {0x0a};
+		scancodes[GLFW_KEY_0] = new int[] {0x0b};
+		scancodes[GLFW_KEY_MINUS] = new int[] {0x0c};
+		scancodes[GLFW_KEY_EQUAL] = new int[] {0x0d};
+		scancodes[GLFW_KEY_BACKSPACE] = new int[] {0x0e};
+		scancodes[GLFW_KEY_TAB] = new int[] {0x0f};
+		scancodes[GLFW_KEY_Q] = new int[] {0x10};
+		scancodes[GLFW_KEY_W] = new int[] {0x11};
+		scancodes[GLFW_KEY_E] = new int[] {0x12};
+		scancodes[GLFW_KEY_R] = new int[] {0x13};
+		scancodes[GLFW_KEY_T] = new int[] {0x14};
+		scancodes[GLFW_KEY_Y] = new int[] {0x15};
+		scancodes[GLFW_KEY_U] = new int[] {0x16};
+		scancodes[GLFW_KEY_I] = new int[] {0x17};
+		scancodes[GLFW_KEY_O] = new int[] {0x18};
+		scancodes[GLFW_KEY_P] = new int[] {0x19};
+		scancodes[GLFW_KEY_LEFT_BRACKET] = new int[] {0x1a};
+		scancodes[GLFW_KEY_RIGHT_BRACKET] = new int[] {0x1b};
+		scancodes[GLFW_KEY_ENTER] = new int[] {0x1c};
+		scancodes[GLFW_KEY_LEFT_CONTROL] = new int[] {0x1d};
+		scancodes[GLFW_KEY_A] = new int[] {0x1e};
+		scancodes[GLFW_KEY_S] = new int[] {0x1f};
+		scancodes[GLFW_KEY_D] = new int[] {0x20};
+		scancodes[GLFW_KEY_F] = new int[] {0x21};
+		scancodes[GLFW_KEY_G] = new int[] {0x22};
+		scancodes[GLFW_KEY_H] = new int[] {0x23};
+		scancodes[GLFW_KEY_J] = new int[] {0x24};
+		scancodes[GLFW_KEY_K] = new int[] {0x25};
+		scancodes[GLFW_KEY_L] = new int[] {0x26};
+		scancodes[GLFW_KEY_SEMICOLON] = new int[] {0x27};
+		scancodes[GLFW_KEY_APOSTROPHE] = new int[] {0x28};
+		scancodes[GLFW_KEY_GRAVE_ACCENT] = new int[] {0x29};
+		scancodes[GLFW_KEY_LEFT_SHIFT] = new int[] {0x2a};
+		scancodes[GLFW_KEY_BACKSLASH] = new int[] {0x2b};
+		scancodes[GLFW_KEY_Z] = new int[] {0x2c};
+		scancodes[GLFW_KEY_X] = new int[] {0x2d};
+		scancodes[GLFW_KEY_C] = new int[] {0x2e};
+		scancodes[GLFW_KEY_V] = new int[] {0x2f};
+		scancodes[GLFW_KEY_B] = new int[] {0x30};
+		scancodes[GLFW_KEY_N] = new int[] {0x31};
+		scancodes[GLFW_KEY_M] = new int[] {0x32};
+		scancodes[GLFW_KEY_COMMA] = new int[] {0x33};
+		scancodes[GLFW_KEY_PERIOD] = new int[] {0x34};
+		scancodes[GLFW_KEY_SLASH] = new int[] {0x35};
+		scancodes[GLFW_KEY_RIGHT_SHIFT] = new int[] {0x36};
+		scancodes[GLFW_KEY_KP_MULTIPLY] = new int[] {0x37};
+		scancodes[GLFW_KEY_LEFT_ALT] = new int[] {0x38};
+		scancodes[GLFW_KEY_SPACE] = new int[] {0x39};
+		scancodes[GLFW_KEY_CAPS_LOCK] = new int[] {0x3a};
+		scancodes[GLFW_KEY_F1] = new int[] {0x3b};
+		scancodes[GLFW_KEY_F2] = new int[] {0x3c};
+		scancodes[GLFW_KEY_F3] = new int[] {0x3d};
+		scancodes[GLFW_KEY_F4] = new int[] {0x3e};
+		scancodes[GLFW_KEY_F5] = new int[] {0x3f};
+		scancodes[GLFW_KEY_F6] = new int[] {0x40};
+		scancodes[GLFW_KEY_F7] = new int[] {0x41};
+		scancodes[GLFW_KEY_F8] = new int[] {0x42};
+		scancodes[GLFW_KEY_F9] = new int[] {0x43};
+		scancodes[GLFW_KEY_F10] = new int[] {0x44};
+		scancodes[GLFW_KEY_NUM_LOCK] = new int[] {0x45};
+		scancodes[GLFW_KEY_SCROLL_LOCK] = new int[] {0x46};
+		scancodes[GLFW_KEY_KP_7] = new int[] {0x47};
+		scancodes[GLFW_KEY_KP_8] = new int[] {0x48};
+		scancodes[GLFW_KEY_KP_9] = new int[] {0x49};
+		scancodes[GLFW_KEY_KP_SUBTRACT] = new int[] {0x4a};
+		scancodes[GLFW_KEY_KP_4] = new int[] {0x4b};
+		scancodes[GLFW_KEY_KP_5] = new int[] {0x4c};
+		scancodes[GLFW_KEY_KP_6] = new int[] {0x4d};
+		scancodes[GLFW_KEY_KP_ADD] = new int[] {0x4e};
+		scancodes[GLFW_KEY_KP_1] = new int[] {0x4f};
+		scancodes[GLFW_KEY_KP_2] = new int[] {0x50};
+		scancodes[GLFW_KEY_KP_3] = new int[] {0x51};
+		scancodes[GLFW_KEY_KP_0] = new int[] {0x52};
+		scancodes[GLFW_KEY_KP_DECIMAL] = new int[] {0x53};
+		scancodes[GLFW_KEY_F11] = new int[] {0x57};
+		scancodes[GLFW_KEY_F12] = new int[] {0x58};
+		scancodes[GLFW_KEY_DOWN] = new int[] {0xe0, 0x50};
+		scancodes[GLFW_KEY_UP] = new int[] {0xe0, 0x48};
+		scancodes[GLFW_KEY_LEFT] = new int[] {0xe0, 0x4b};
+		scancodes[GLFW_KEY_RIGHT] = new int[] {0xe0, 0x4d};
+		scancodes[GLFW_KEY_RIGHT_ALT] = new int[] {0xe0, 0x38};
+		scancodes[GLFW_KEY_KP_ENTER] = new int[] {0xe0, 0x1c};
+		scancodes[GLFW_KEY_RIGHT_CONTROL] = new int[] {0xe0, 0x1d};
+		scancodes[GLFW_KEY_KP_DIVIDE] = new int[] {0xe0, 0x35};
+		scancodes[GLFW_KEY_HOME] = new int[] {0xe0, 0x47};
+		scancodes[GLFW_KEY_PAGE_UP] = new int[] {0xe0, 0x49};
+		scancodes[GLFW_KEY_END] = new int[] {0xe0, 0x50};
+		scancodes[GLFW_KEY_PAGE_DOWN] = new int[] {0xe0, 0x51};
+		scancodes[GLFW_KEY_INSERT] = new int[] {0xe0, 0x52};
+		scancodes[GLFW_KEY_DELETE] = new int[] {0xe0, 0x53};
+		scancodes[GLFW_KEY_MENU] = new int[] {0xe0, 0x5d};
 	}
 }
--- src/main/java/mcvmcomputers/mixins/ServerMixin.java
+++ src/main/java/mcvmcomputers/mixins/ServerMixin.java
@@ -46,36 +46,27 @@ public class ServerMixin {
 	@Inject(at = @At("HEAD"), method = "tick")
 	protected void tick(CallbackInfo ci) {
 		for(TabletOrder order : MainMod.orders.values()) {
-			if(order.currentStatus == OrderStatus.ORDER_CHEST_ARRIVAL_SOON) {
-				order.tickCount += tickTime / 1000f;
-				if(order.tickCount > 5) {
-					order.currentStatus = OrderStatus.ORDER_CHEST_ARRIVED;
-					order.tickCount = 0;
+			switch (order.currentStatus) {
+				case ORDER_CHEST_ARRIVAL_SOON, PAYMENT_CHEST_ARRIVAL_SOON -> {
+					order.tickCount += tickTime / 1000f;
+					if (order.tickCount > 5) {
+						order.currentStatus = OrderStatus.values()[order.currentStatus.ordinal() + 1];
+						order.tickCount = 0;
+					}
 				}
-			}else if(order.currentStatus == OrderStatus.PAYMENT_CHEST_ARRIVAL_SOON) {
-				order.tickCount += tickTime / 1000f;
-				if(order.tickCount > 5) {
-					order.currentStatus = OrderStatus.PAYMENT_CHEST_ARRIVED;
-					order.tickCount = 0;
+				case ORDER_CHEST_RECEIVED -> {
+					order.tickCount += tickTime / 1000f;
+					if (order.tickCount > 0.25) {
+						MainMod.orders.remove(UUID.fromString(order.orderUUID));
+					}
 				}
-			}else if(order.currentStatus == OrderStatus.ORDER_CHEST_RECEIVED) {
-				order.tickCount += tickTime / 1000f;
-				if(order.tickCount > 0.25) {
-					MainMod.orders.remove(UUID.fromString(order.orderUUID));
-				}
-			}else if(order.currentStatus == OrderStatus.ORDER_CHEST_ARRIVED) {
-				if(!order.entitySpawned) {
-					PlayerEntity p = playerManager.getPlayer(UUID.fromString(order.orderUUID));
-					World w = p.world;
-					w.spawnEntity(new EntityDeliveryChest(w, new Vec3d(p.getX(), p.getY(), p.getZ()), p.getUuid()));
-					order.entitySpawned = true;
-				}
-			}else if(order.currentStatus == OrderStatus.PAYMENT_CHEST_ARRIVED) {
-				if(!order.entitySpawned) {
-					PlayerEntity p = playerManager.getPlayer(UUID.fromString(order.orderUUID));
-					World w = p.world;
-					w.spawnEntity(new EntityDeliveryChest(w, new Vec3d(p.getX(), p.getY(), p.getZ()), p.getUuid()));
-					order.entitySpawned = true;
+				case ORDER_CHEST_ARRIVED, PAYMENT_CHEST_ARRIVED -> {
+					if (!order.entitySpawned) {
+						PlayerEntity p = playerManager.getPlayer(UUID.fromString(order.orderUUID));
+						World w = p.world;
+						w.spawnEntity(new EntityDeliveryChest(w, new Vec3d(p.getX(), p.getY(), p.getZ()), p.getUuid()));
+						order.entitySpawned = true;
+					}
 				}
 			}
 			
